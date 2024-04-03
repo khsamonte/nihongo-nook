6c5d4186d9d671b7dcba286eaf551b383ceb08b8
Ken Samonte <khsamonte@gmail.com>
2024-04-03 09:56:49 +0800

    Fixed router issue

--- src/utils/preprocessMarkdown.js
+++ src/utils/preprocessMarkdown.js
@@ -1,22 +1,37 @@
 export const preprocessMarkdown = (markdown) => {
-  let processedMarkdown = markdown;
+	let processedMarkdown = markdown;
 
-  const namesToHighlight = ["咲太", "麻衣"];
+	const namesToHighlight = [
+		"咲太",
+		"梓川",
+		"麻衣",
+		"桜島",
+		"カエデ",
+		"国見",
+		"佑真",
+		"沙希",
+		"上里",
+		"南条",
+		"文香",
+		"理央",
+		"牧之",
+		"原翔子",
+	];
 
-  namesToHighlight.forEach((name) => {
-    processedMarkdown = processedMarkdown.replace(
-      new RegExp(name, "g"),
-      `[${name}](character-name)`
-    );
-  });
+	namesToHighlight.forEach((name) => {
+		processedMarkdown = processedMarkdown.replace(
+			new RegExp(name, "g"),
+			`[${name}](character-name)`
+		);
+	});
 
-  // namesToHighlight.forEach((name) => {
-  //   const regex = new RegExp(name, "g");
-  //   processedMarkdown = processedMarkdown?.replace(
-  //     regex,
-  //     `<span class="character-name">${name}</span>`
-  //   );
-  // });
+	// namesToHighlight.forEach((name) => {
+	//   const regex = new RegExp(name, "g");
+	//   processedMarkdown = processedMarkdown?.replace(
+	//     regex,
+	//     `<span class="character-name">${name}</span>`
+	//   );
+	// });
 
-  return processedMarkdown;
+	return processedMarkdown;
 };
