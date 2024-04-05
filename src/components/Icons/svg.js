141fa6816018d20d245518342b11fffaa2f801d1
Ken Samonte <khsamonte@gmail.com>
2024-04-05 18:28:33 +0800

    Added Sakura Dokkai 1-3

---
+++ src/components/Icons/svg.js
@@ -0,0 +1,27 @@
+export const DownIcon = () => {
+	return (
+		<svg
+			fill="rgb(209, 203, 193)"
+			width="14px"
+			height="14px"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z" />
+		</svg>
+	);
+};
+
+export const RightIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="rgb(209, 203, 193)"
+			width="14px"
+			height="14px"
+			viewBox="0 0 24 24"
+		>
+			<path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z" />
+		</svg>
+	);
+};
--- src/components/MarkdownContent.jsx
+++ src/components/MarkdownContent.jsx
@@ -1,110 +1,157 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
+import gfm from "remark-gfm";
 
 import { preprocessMarkdown } from "../utils/preprocessMarkdown";
 
 const MarkdownContent = ({ markdownFile }) => {
-  const [markdown, setMarkdown] = useState("");
-  const [loading, setLoading] = useState(false);
+	const [markdown, setMarkdown] = useState("");
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (!markdownFile) {
-      setMarkdown("");
-      return; // Exit early
-    }
+	useEffect(() => {
+		if (!markdownFile) {
+			setMarkdown("");
+			return; // Exit early
+		}
 
-    setLoading(true); // Start loading
+		setLoading(true); // Start loading
 
-    fetch(markdownFile)
-      .then((response) => response.text())
-      .then((text) => {
-        setMarkdown(preprocessMarkdown(text));
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching markdown:", error);
-        setLoading(false); // Stop loading also in case of error
-      });
-  }, [markdownFile]);
+		fetch(markdownFile)
+			.then((response) => response.text())
+			.then((text) => {
+				setMarkdown(preprocessMarkdown(text));
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.error("Error fetching markdown:", error);
+				setLoading(false); // Stop loading also in case of error
+			});
+	}, [markdownFile]);
 
-  const renderMarkdown = () => {
-    if (loading) {
-      return <p>Loading...</p>;
-    } else if (!markdown) {
-      return <p>Empty.</p>;
-    } else {
-      return (
-        <ReactMarkdown
-          children={markdown}
-          components={{
-            a: ({ node, ...props }) => {
-              if (props.href === "character-name") {
-                // This is where you'd return your custom component or styling
-                return <NameSpan>{props.children}</NameSpan>;
-              } else if (props.href === "location-name") {
-                return <LocationSpan>{props.children}</LocationSpan>;
-              }
-              // Fallback for other links
-              return <a {...props}>{props.children}</a>;
-            },
-          }}
-        />
-      );
-    }
-  };
+	const renderMarkdown = () => {
+		if (loading) {
+			return <p>Loading...</p>;
+		} else if (!markdown) {
+			return <p>Empty.</p>;
+		} else {
+			return (
+				<ReactMarkdown
+					remarkPlugins={[gfm]}
+					children={markdown}
+					components={{
+						a: ({ node, ...props }) => {
+							if (props.href === "character-name") {
+								// This is where you'd return your custom component or styling
+								return <NameSpan>{props.children}</NameSpan>;
+							} else if (props.href === "location-name") {
+								return <LocationSpan>{props.children}</LocationSpan>;
+							}
+							// Fallback for other links
+							return <a {...props}>{props.children}</a>;
+						},
+					}}
+				/>
+			);
+		}
+	};
 
-  return <MarkdownContainer>{renderMarkdown()}</MarkdownContainer>;
+	return <MarkdownContainer>{renderMarkdown()}</MarkdownContainer>;
 };
 
 const MarkdownContainer = styled.div`
-  font-family: "NotoSansJP-Light";
-  font-weight: 300;
-  margin: 0 0 1.5em;
-  line-height: 1.5;
-  list-style: none;
+	font-family: "NotoSansJP-Light";
+	font-weight: 300;
+	margin: 0 0 1.5em;
+	line-height: 1.5;
+	list-style: none;
 
-  h2 {
-    font-family: "NotoSansJP-Light";
-    font-size: 1.625em;
-    font-weight: 300;
-    margin: 0 0 0.75em;
-  }
+	h2 {
+		font-family: "NotoSansJP-Light";
+		font-size: 1.625em;
+		font-weight: 300;
+		margin: 0 0 0.75em;
+	}
 
-  h3 {
-    font-family: "NotoSansJP-Light";
-    font-size: 1.375em;
-    font-weight: 300;
-    margin: 0 0 0.75em;
-  }
+	h3 {
+		font-family: "NotoSansJP-Light";
+		font-size: 1.375em;
+		font-weight: 300;
+		margin: 0 0 1.5em;
+	}
 
-  hr {
-    border: none;
-    border-top: 1px solid rgb(63, 65, 65);
-    margin: 0 0 1.5em;
-  }
+	hr {
+		border: none;
+		border-top: 1px solid rgb(63, 65, 65);
+		margin: 0 0 1.5em;
+	}
 
-  &.character-name {
-    color: orange;
-  }
+	&.character-name {
+		color: orange;
+	}
 
-  p,
-  ul {
-    font-family: "NotoSansJP-Light";
-    font-size: 1.25em;
-    font-weight: 300;
-    margin: 0 0 1.5em;
-    line-height: 1.5;
-    list-style: none;
-  }
+	p,
+	ul {
+		font-family: "NotoSansJP-Light";
+		font-size: 1.25em;
+		font-weight: 300;
+		margin: 0 0 1.5em;
+		line-height: 1.5;
+		list-style: none;
+	}
+
+	/* Basic Table Styling */
+	table {
+		border-collapse: collapse;
+		border-top: 1px solid rgb(68, 70, 70);
+		font-size: 1.25em;
+		margin: 0 0 1.5em;
+		width: 100%;
+	}
+
+	pre {
+		border: 1px solid rgb(68, 70, 70);
+		font-size: 1.25em;
+		margin: 0 0 1.5em;
+		padding: 8px;
+
+		white-space: pre-wrap; /* Since CSS 2.1 */
+		white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+		white-space: -pre-wrap; /* Opera 4-6 */
+		white-space: -o-pre-wrap; /* Opera 7 */
+		word-wrap: break-word; /* Internet Explorer 5.5+ */
+	}
+
+	th,
+	td {
+		padding: 8px;
+		text-align: left;
+		border-bottom: 1px solid rgb(68, 70, 70);
+		border-right: 1px solid rgb(68, 70, 70);
+	}
+
+	th:first-child,
+	td:first-child {
+		border-left: 1px solid rgb(68, 70, 70);
+	}
+
+	/* Style for the first column */
+	th:first-child,
+	td:first-child {
+		text-align: left;
+	}
+
+	img {
+		max-width: 100%;
+	}
 `;
 
 const NameSpan = styled.span`
-  color: rgb(239, 132, 75);
+	color: rgb(239, 132, 75);
 `;
 
 const LocationSpan = styled.span`
-  color: blueviolet;
+	color: blueviolet;
 `;
 
 export default MarkdownContent;
